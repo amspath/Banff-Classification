75919125a43d433be8f2c2355c1439f2345d9629
Giorgio Buzzanca <g.buzzanca@lumc.nl>
2023-11-16 15:49:58 +0100

    src(model): simplify attention

--- model/transformer.py
+++ model/transformer.py
@@ -11,42 +11,6 @@ from timm.models.vision_transformer import LayerScale, DropPath
 from timm.layers.mlp import Mlp
 
 
-class ScaledDotProductAttention(nn.Module):
-    """
-    Scaled Dot-Product Attention proposed in "Attention Is All You Need"
-    Compute the dot products of the query with all keys, divide each by sqrt(dim),
-    and apply a softmax function to obtain the weights on the values
-
-    Args: dim, mask
-        dim (int): dimention of attention
-        mask (torch.Tensor): tensor containing indices to be masked
-
-    Inputs: query, key, value, mask
-        - **query** (batch, q_len, d_model): tensor containing projection vector for decoder.
-        - **key** (batch, k_len, d_model): tensor containing projection vector for encoder.
-        - **value** (batch, v_len, d_model): tensor containing features of the encoded input sequence.
-        - **mask** (-): tensor containing indices to be masked
-
-    Returns: context, attn
-        - **context**: tensor containing the context vector from attention mechanism.
-        - **attn**: tensor containing the attention (alignment) from the encoder outputs.
-    """
-    def __init__(self, dim: int):
-        super(ScaledDotProductAttention, self).__init__()
-        self.sqrt_dim = np.sqrt(dim)
-
-    def forward(self, query: Tensor, key: Tensor, value: Tensor, mask: Optional[Tensor] = None) \
-            -> Tuple[Tensor, Tensor]:
-        score = torch.bmm(query, key.transpose(1, 2)) / self.sqrt_dim
-
-        if mask is not None:
-            score.masked_fill_(mask.view(score.size()), -float('Inf'))
-
-        attn = F.softmax(score, -1)
-        context = torch.bmm(attn, value)
-        return context, attn
-
-
 class Attention(nn.Module):
     def __init__(
             self,
@@ -70,7 +34,6 @@ class Attention(nn.Module):
         self.attn_drop = nn.Dropout(attn_drop)
         self.proj = nn.Linear(dim, dim, bias=False)
         self.proj_drop = nn.Dropout(proj_drop)
-        self.scaled_dot_product_attention = ScaledDotProductAttention(self.head_dim)
 
     def forward(self, x, mask=None):
         B, N, C = x.shape
@@ -81,7 +44,11 @@ class Attention(nn.Module):
         if mask is not None:
             mask = mask.unsqueeze(1).repeat(1, self.num_heads, 1, 1)
 
-        x, _ = self.scaled_dot_product_attention(q, k, v, mask)
+        q = q * self.scale
+        attn = q @ k.transpose(-2, -1)
+        attn = attn.softmax(dim=-1)
+        attn.masked_fill_(mask.view(attn.size()), -float('Inf'))
+        x = attn @ v
 
         x = x.transpose(1, 2).reshape(B, N, C)
         x = self.proj(x)
